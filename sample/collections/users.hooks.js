459bf276c5941a9ac647fde0f3ed0e0ac5a4dfcc
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-10-29 19:11:30 +0100

    hooks: wip

--- sample/collections/users.hooks.js
+++ sample/collections/users.hooks.js
@@ -1,7 +1,20 @@
 
-restQuery = require( '../../lib/restQuery.js' ) ;
+
+var restQuery = require( '../../lib/restQuery.js' ) ;
+var logger = require( 'logger-kit' ).global.use( 'users-hooks' ) ;
+
+
 
 module.exports = {
+	
+	beforeCreate: function( incomingDocument , attachmentStreams , context , callback ) {
+		logger.info( '>>>>>>>>>> beforeCreate!!!' ) ;
+		
+		context.performer.getUser( function( error , user ) {
+			logger.info( '>>>>>>>>>> beforeCreate: %I' , user ) ;
+			callback() ;
+		} ) ;
+	} ,
 	
 	beforeCreateDocument: function( rawDocument ) {
 		if ( ! rawDocument.login ) { rawDocument.login = rawDocument.email ; }
